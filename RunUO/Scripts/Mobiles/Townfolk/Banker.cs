d9d090f161449eafa35552d66f29353a0164deb6
Nevan McGill <nmcgill@zynga.com>
2013-09-15 19:29:51 -0700

    Bankers respond to ascii

--- RunUO/Scripts/Mobiles/Townfolk/Banker.cs
+++ RunUO/Scripts/Mobiles/Townfolk/Banker.cs
@@ -234,152 +234,83 @@ namespace Server.Mobiles
                     return;
                 }
 
-				for ( int i = 0; i < e.Keywords.Length; ++i )
+				if (Insensitive.Speech(e.Speech, "withdraw")) // *withdraw*
 				{
-					int keyword = e.Keywords[i];
+					e.Handled = true;
 
-					switch ( keyword )
+					if ( e.Mobile.Criminal )
 					{
-						case 0x0000: // *withdraw*
+						this.Say( true, "I will not do business with a criminal!" ); // I will not do business with a criminal!
+						return;
+					}
+
+					string[] split = e.Speech.Split( ' ' );
+
+					if ( split.Length >= 2 )
+					{
+						int amount;
+
+						try
 						{
-							e.Handled = true;
-
-							if ( e.Mobile.Criminal )
-							{
-								this.Say( true, "I will not do business with a criminal!" ); // I will not do business with a criminal!
-								break;
-							}
-
-							string[] split = e.Speech.Split( ' ' );
-
-							if ( split.Length >= 2 )
-							{
-								int amount;
-
-								try
-								{
-									amount = Convert.ToInt32( split[1] );
-								}
-								catch
-								{
-									break;
-								}
-
-								if ( amount > 5000 )
-								{
-                                    this.Say(true, "Thou canst not withdraw so much at one time!"); // Thou canst not withdraw so much at one time!
-								}
-								else if ( amount > 0 )
-								{
-									BankBox box = e.Mobile.FindBankNoCreate();
-
-									if ( box == null || !box.ConsumeTotal( typeof( Gold ), amount ) )
-									{
-                                        this.Say(true, "Ah, art thou trying to fool me? Thou hast not so much gold!"); // Ah, art thou trying to fool me? Thou hast not so much gold!
-									}
-									else
-									{
-										e.Mobile.AddToBackpack( new Gold( amount ) );
-
-                                        this.Say(true, "Thou hast withdrawn gold from thy account."); // Thou hast withdrawn gold from thy account.
-									}
-								}
-							}
-
-							break;
+							amount = Convert.ToInt32( split[1] );
 						}
-						case 0x0001: // *balance*
+						catch
 						{
-							e.Handled = true;
+							return;
+						}
 
-							if ( e.Mobile.Criminal )
-							{
-                                this.Say(true, "I will not do business with a criminal!"); // I will not do business with a criminal!
-								break;
-							}
-
+						if ( amount > 5000 )
+						{
+                            this.Say(true, "Thou canst not withdraw so much at one time!"); // Thou canst not withdraw so much at one time!
+						}
+						else if ( amount > 0 )
+						{
 							BankBox box = e.Mobile.FindBankNoCreate();
 
-							if ( box != null )
-								this.Say( true, String.Format("Thy current bank balance is {0}", box.TotalGold.ToString()) ); // Thy current bank balance is ~1_AMOUNT~ gold.
+							if ( box == null || !box.ConsumeTotal( typeof( Gold ), amount ) )
+							{
+                                this.Say(true, "Ah, art thou trying to fool me? Thou hast not so much gold!"); // Ah, art thou trying to fool me? Thou hast not so much gold!
+							}
 							else
-								this.Say( true, "They current bank balance is 0 gold." ); // Thy current bank balance is ~1_AMOUNT~ gold.
+							{
+								e.Mobile.AddToBackpack( new Gold( amount ) );
 
-							break;
+                                this.Say(true, "Thou hast withdrawn gold from thy account."); // Thou hast withdrawn gold from thy account.
+							}
 						}
-						case 0x0002: // *bank*
-						{
-							e.Handled = true;
-
-							if ( e.Mobile.Criminal )
-							{
-								this.Say( true, "Thou art a criminal and cannot access thy bank box." ); // Thou art a criminal and cannot access thy bank box.
-								break;
-							}
-
-							e.Mobile.BankBox.Open();
-
-							break;
-						}
-						/*case 0x0003: // *check*
-						{
-							e.Handled = true;
-
-							if ( e.Mobile.Criminal )
-							{
-								this.Say( true, "I will not do business with a criminal!" ); // I will not do business with a criminal!
-								break;
-							}
-
-							string[] split = e.Speech.Split( ' ' );
-
-							if ( split.Length >= 2 )
-							{
-								int amount;
-
-								try
-								{
-									amount = Convert.ToInt32( split[1] );
-								}
-								catch
-								{
-									break;
-								}
-
-								if ( amount < 5000 )
-								{
-									this.Say( 1010006 ); // We cannot create checks for such a paltry amount of gold!
-								}
-								else if ( amount > 1000000 )
-								{
-									this.Say( 1010007 ); // Our policies prevent us from creating checks worth that much!
-								}
-								else
-								{
-									BankCheck check = new BankCheck( amount );
-
-									BankBox box = e.Mobile.BankBox;
-
-									if ( !box.TryDropItem( e.Mobile, check, false ) )
-									{
-										this.Say( 500386 ); // There's not enough room in your bankbox for the check!
-										check.Delete();
-									}
-									else if ( !box.ConsumeTotal( typeof( Gold ), amount ) )
-									{
-										this.Say( 500384 ); // Ah, art thou trying to fool me? Thou hast not so much gold!
-										check.Delete();
-									}
-									else
-									{
-										this.Say( 1042673, AffixType.Append, amount.ToString(), "" ); // Into your bank box I have placed a check in the amount of:
-									}
-								}
-							}
-
-							break;
-						}*/
 					}
+				}
+
+				if ((Insensitive.Speech(e.Speech, "balance"))) // *balance*
+				{
+					e.Handled = true;
+
+					if ( e.Mobile.Criminal )
+					{
+                        this.Say(true, "I will not do business with a criminal!"); // I will not do business with a criminal!
+						return;
+					}
+
+					BankBox box = e.Mobile.FindBankNoCreate();
+
+					if ( box != null )
+						this.Say( true, String.Format("Thy current bank balance is {0}", box.TotalGold.ToString()) ); // Thy current bank balance is ~1_AMOUNT~ gold.
+					else
+						this.Say( true, "They current bank balance is 0 gold." ); // Thy current bank balance is ~1_AMOUNT~ gold.
+
+				}
+
+				if (Insensitive.Speech(e.Speech, "bank")) // *bank*
+				{
+					e.Handled = true;
+
+					if ( e.Mobile.Criminal )
+					{
+						this.Say( true, "Thou art a criminal and cannot access thy bank box." ); // Thou art a criminal and cannot access thy bank box.
+						return;
+					}
+
+					e.Mobile.BankBox.Open();
 				}
 			}
 
