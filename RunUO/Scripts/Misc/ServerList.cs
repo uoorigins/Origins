c1a6a4af01d33a321a889af706be9f9857f6e2d2
Nevan McGill <nmcgill@zynga.com>
2013-09-20 22:13:57 -0700

    Shard rename

--- RunUO/Scripts/Misc/ServerList.cs
+++ RunUO/Scripts/Misc/ServerList.cs
@@ -38,7 +38,7 @@ namespace Server.Misc
 		 */
 
 		public static readonly string Address = null;
-		public static readonly string ServerName = "Origins";
+		public static readonly string ServerName = "Origins Beta";
 
 		public static readonly bool AutoDetect = true;
 
