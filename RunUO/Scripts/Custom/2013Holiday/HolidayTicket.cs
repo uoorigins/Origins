574faa6641535c768357269fcfca94131d20bed9
UO: Origins Pre-T2A RunUO Server Package <exodus@uoorigins.com>
2013-12-16 22:53:53 -0800

    Christmas Changes

--- RunUO/Scripts/Custom/2013Holiday/HolidayTicket.cs
+++ RunUO/Scripts/Custom/2013Holiday/HolidayTicket.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Server.Mobiles;
+using System;
 using System.Collections.Generic;
 
 namespace Server.Misc
@@ -6,10 +7,26 @@ namespace Server.Misc
     class HolidayTicket : Item
     {
 
-        [Constructable]
-        public HolidayTicket(Mobile mobile) : base(0x14F0)
+        private PlayerMobile m_Player;
+
+        [CommandProperty(AccessLevel.Administrator)]
+        public PlayerMobile Player
         {
-            Name = "This is prize ticket! Bring this ticket to the nearest bank with a holiday tree. This ticket will only work for YOU, so don’t give it away!";
+            get
+            {
+                return m_Player;
+            }
+            set
+            {
+                m_Player = value;
+            }
+        }
+
+        [Constructable]
+        public HolidayTicket(PlayerMobile mobile) : base(0x14F0)
+        {
+            m_Player = mobile;
+            Name = "This is a prize ticket! Bring this ticket to the nearest Santa at a city bank. This ticket will only work for YOU, so don’t give it away!";
             Weight = 1.0;
             LootType = LootType.Blessed;
         }
@@ -24,6 +41,8 @@ namespace Server.Misc
             base.Serialize(writer);
 
             writer.Write((int)0); // version
+
+            writer.WriteMobile(m_Player);
         }
 
         public override void Deserialize(GenericReader reader)
@@ -31,6 +50,8 @@ namespace Server.Misc
             base.Deserialize(reader);
 
             int version = reader.ReadInt();
+
+            m_Player = reader.ReadMobile() as PlayerMobile;
         }
     }
 }
