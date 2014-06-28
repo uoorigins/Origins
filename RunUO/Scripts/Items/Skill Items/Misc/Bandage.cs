d6bf98aa0cebffcbf92fa2ab2122831dcba22264
uoorigins <exodus@uoorigins.com>
2014-06-28 12:51:45 -0400

    False anatomy check

--- RunUO/Scripts/Items/Skill Items/Misc/Bandage.cs
+++ RunUO/Scripts/Items/Skill Items/Misc/Bandage.cs
@@ -237,7 +237,7 @@ namespace Server.Items
 				double anatomy = m_Healer.Skills[secondarySkill].Value;
 				double chance = ((healing - 68.0) / 50.0) - (m_Slips * 0.02);
 
-				if (( (checkSkills = (healing >= 80.0 && anatomy >= 80.0)) && chance > Utility.RandomDouble() )
+				if (( (checkSkills = healing >= 80.0 && chance > Utility.RandomDouble() )
 				      || ( Core.SE && petPatient is Factions.FactionWarHorse && petPatient.ControlMaster == m_Healer) )	//TODO: Dbl check doesn't check for faction of the horse here?
 				{
 					if ( m_Patient.Map == null || !m_Patient.Map.CanFit( m_Patient.Location, 16, false, false ) )
