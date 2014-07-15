9d4c083ef6eeca33cb6ac489e130e559a2ba0333
nmcgill <nmcgill@users.noreply.github.com>
2014-07-14 21:53:05 -0400

    Charged Dye Tub

---
+++ RunUO/Scripts/Custom/ChargedDyeTub.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Server.Items
+{
+    public class ChargedDyeTub : DyeTub
+    {
+        public override bool AllowBolts { get { return false; } }
+
+        private int m_Charges;
+
+        [CommandProperty( AccessLevel.GameMaster )]
+        public int Charges
+        {
+            get { return m_Charges; }
+            set { m_Charges = value; }
+        }
+
+        [Constructable]
+        public ChargedDyeTub() :this(3)
+        {
+        }
+
+        [Constructable]
+        public ChargedDyeTub(int charge)
+        {
+            Charges = charge;
+        }
+
+        public ChargedDyeTub( Serial serial )
+            : base( serial )
+        {
+        }
+
+        protected override void AfterDye(Mobile from)
+        {
+            Charges--;
+            if ( Charges < 1 )
+            {
+                from.SendAsciiMessage("The dye tub has run out of charges.");
+                Delete();
+            }
+            else
+            {
+                base.AfterDye(from);
+            }
+        }
+
+        public override void Serialize( GenericWriter writer )
+        {
+            base.Serialize( writer );
+
+            writer.Write( (int)0 ); // version
+
+            writer.Write( (int)m_Charges );
+        }
+
+        public override void Deserialize( GenericReader reader )
+        {
+            base.Deserialize( reader );
+
+            int version = reader.ReadInt();
+
+            m_Charges = reader.ReadInt();
+        }
+    }
+}
