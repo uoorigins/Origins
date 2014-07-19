ed1e3f566aa4ea591405104ac6d46e13bc10d36a
nmcgill <nmcgill@users.noreply.github.com>
2014-07-18 22:59:25 -0400

    Spawn Barrels Always

---
+++ RunUO/Scripts/Custom/SpawnBarrels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Server.Commands;
+using System.IO;
+
+namespace Server.Misc
+{
+    public static class SpawnBarrels
+    {
+        private const bool m_Enabled = true;
+        public static bool Enabled { get { return m_Enabled; } }
+
+        public static void Initialize()
+        {
+            if ( Enabled )
+            {
+                Decorate.Generate( "Data/Decoration/Britannia/Barrels", Map.Felucca );
+            }
+        }
+    }
+}
